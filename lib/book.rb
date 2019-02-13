6ea2e848dc271b0eef2d911f0b71153bf252fce2
wipegup <wipegup@gmail.com>
2019-02-13 09:16:37 -0700

    passing attributes test

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,11 @@
 class Book
+  attr_reader :author_first_name, :author_last_name,
+              :title, :publication_date
   def initialize(info_hash)
-
+    @author_first_name = info_hash[:author_first_name]
+    @author_last_name = info_hash[:author_last_name]
+    @title = info_hash[:title]
+    @publication_date = info_hash[:publication_date][-4..-1]
   end
 
 
