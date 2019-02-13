7bd76bda0a53bcec2aa6bace6273387885a21792
wipegup <wipegup@gmail.com>
2019-02-13 09:18:05 -0700

    Add setup and test it exists for author

--- test/author_test.rb
+++ test/author_test.rb
@@ -1,6 +1,14 @@
 require './test/test_helper'
+require './lib/author'
 
 class AuthorTest < Minitest::Test
+  def setup
+    @author = Author.new({first_name: "N.K.",
+      last_name: "Jemisin"})
+  end
 
+  def test_it_exists
+    assert_instance_of Author, @author
+  end
 
 end
