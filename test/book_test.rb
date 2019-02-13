d8118886adfe41a7cfd7fe9e2f33e8b73002c87a
wipegup <wipegup@gmail.com>
2019-02-13 09:07:02 -0700

    Add setup and test it exists to book test

--- test/book_test.rb
+++ test/book_test.rb
@@ -2,5 +2,17 @@ require './test/test_helper'
 
 class BookTest < Minitest::Test
 
+    def setup
+      @book = Book.new({author_first_name: "Harper",
+                        author_last_name: "Lee",
+                        title: "To Kill a Mockingbird",
+                        publication_date: "July 11, 1960"})
+    end
+
+    def test_it_exists
+
+      assert_instance_of Book, @book
+
+    end
 
 end
