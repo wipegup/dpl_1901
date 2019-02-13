66f16ae23e0c8781a2221d8b286a690d8ac7d14a
wipegup <wipegup@gmail.com>
2019-02-13 09:25:46 -0700

    Add test for adding books

--- test/author_test.rb
+++ test/author_test.rb
@@ -14,4 +14,20 @@ class AuthorTest < Minitest::Test
   def test_it_starts_with_empty_books_array
     assert_equal [], @author.books
   end
+
+  def test_it_can_add_books
+    @author.add_book("The Fifth Season", "November 3, 2015")
+    @author.add_book("The Hundred Thousand Kingdoms", "2010")
+
+    book_1 = Book.new({author_first_name: "N.K.",
+                      author_last_name: "Jemisin",
+                      title: "The Fifth Season",
+                      publication_date: "November 3, 2015"})
+
+    book_2 = Book.new({author_first_name: "N.K.",
+                      author_last_name: "Jemisin",
+                      title: "The Hundred Thousand Kingdoms",
+                      publication_date: "2010"})
+
+    assert_equal [book_1, book_2], @author.books
 end
