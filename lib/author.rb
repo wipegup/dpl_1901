0b60725d351064f73eda17c0eb4ded61580d408b
wipegup <wipegup@gmail.com>
2019-02-13 09:31:18 -0700

    Add add book method -- Failing b/c of object comparison

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,19 @@
+require './lib/book'
 class Author
   attr_reader :books
   def initialize(info_hash)
     @books = []
+    @author_first_name = info_hash[:first_name]
+    @author_last_name = info_hash[:last_name]
+  end
+
+  def add_book(title, publication_date)
+    book = Book.new({author_first_name: @author_first_name,
+                    author_last_name: @author_last_name,
+                    title: title,
+                    publication_date: publication_date})
+
+    @books << book
+
   end
 end
