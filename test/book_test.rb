38b013028e63e08bfae1254971648f802134db23
wipegup <wipegup@gmail.com>
2019-02-13 09:11:29 -0700

    Add test for attributes

--- test/book_test.rb
+++ test/book_test.rb
@@ -10,9 +10,14 @@ class BookTest < Minitest::Test
     end
 
     def test_it_exists
-
       assert_instance_of Book, @book
+    end
 
+    def test_it_has_attributes
+      assert_equal "Harper", @book.author_first_name
+      assert_equal "Lee", @book.author_last_name
+      assert_equal "To Kill a Mockingbird", @book.title
+      assert_equal "1960", @book.publication_date
     end
 
 end
